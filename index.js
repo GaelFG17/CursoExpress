ffbf0822c46bbc232e40e83bea3ff732d8958575
GaelFG17 <gaelfg1720@gmail.com>
2025-01-24 17:27:55 -0600

    Configuracion de rutas

--- index.js
+++ index.js
@@ -6,6 +6,17 @@ app.get('/', (req, res) => {
   res.send('Hola desde mi server de express');
 })
 
+app.get('/nueva-ruta', (req, res) => {
+  res.send('Hola, soy una nueva ruta');
+})
+
+app.get('/productos', (req, res) => {
+  res.json({
+    name : 'Producto 1',
+    precio: 1500
+  });
+})
+
 app.listen(port, () => {
   console.log(`Server corriendo en http://localhost:${port}`);
 });
