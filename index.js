42eb6dcada1394d31debb920e6fc17a84354c336
GaelFG17 <gaelfg1720@gmail.com>
2025-01-24 16:53:34 -0600

    Configuracion inicial del server de express

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('My app')
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hola desde mi server de express');
+})
+
+app.listen(port, () => {
+  console.log(`Server corriendo en http://localhost:${port}`);
+});
