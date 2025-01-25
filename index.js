6b91e9b89571e76d4fedf54557466df9923d53fd
GaelFG17 <gaelfg1720@gmail.com>
2025-01-25 13:45:21 -0600

    Uso de faker

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { faker } = require('@faker-js/faker');
+const { fa } = require('faker/lib/locales');
 
 const app = express();
 const port = 3000;
@@ -13,16 +14,17 @@ app.get('/nueva-ruta', (req, res) => {
 })
 
 app.get('/products', (req, res) => {
-  res.json([
-    {
-    name : 'Producto 1',
-    precio: 1500
-    },
-    {
-      name : 'Producto 2',
-      precio: 2500
-    }
-  ]);
+  const products = [];
+  const { size } = req.query;
+  const limit = size || 10;
+  for (let index = 0; index < limit; index++) {
+    products.push({
+      name: faker.commerce.productName(),
+      price: parseInt(faker.commerce.price(), 10),
+      image : faker.image.url()
+    });
+  }
+  res.json(products);
 })
 
 app.get('/products/:id', (req, res) => {
