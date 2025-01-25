2a4ef976fa4d42e44f08f0e498b06d6df67f5c4b
GaelFG17 <gaelfg1720@gmail.com>
2025-01-25 13:46:12 -0600

    Evitar choque de routing

--- index.js
+++ index.js
@@ -27,6 +27,10 @@ app.get('/products', (req, res) => {
   res.json(products);
 })
 
+app.get('products/filter', (req, res) => {
+  res.send('Yo soy un filter');
+})
+
 app.get('/products/:id', (req, res) => {
   const { id } = req.params;
   res.json({
