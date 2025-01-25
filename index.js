175b9d8669fe54047925c02b153d9bfb0a690caa
GaelFG17 <gaelfg1720@gmail.com>
2025-01-25 13:16:28 -0600

    Metodo para recibir param por GET

--- index.js
+++ index.js
@@ -10,11 +10,26 @@ app.get('/nueva-ruta', (req, res) => {
   res.send('Hola, soy una nueva ruta');
 })
 
-app.get('/productos', (req, res) => {
-  res.json({
+app.get('/products', (req, res) => {
+  res.json([
+    {
     name : 'Producto 1',
     precio: 1500
-  });
+    },
+    {
+      name : 'Producto 2',
+      precio: 2500
+    }
+  ]);
+})
+
+app.get('/products/:id', (req, res) => {
+  const { id } = req.params;
+  res.json({
+    id,
+    name: 'Producto 1',
+    precio: 1500
+  })
 })
 
 app.listen(port, () => {
