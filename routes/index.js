0bc34c494d050579766174a440f8c42423b7ca5d
GaelFG17 <gaelfg1720@gmail.com>
2025-01-27 13:39:20 -0600

    Cracion del modulo para el routing

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
+const { mode } = require('crypto-js');
 const productsRouter = require('./productsRouter');
 
 function routerApi(){
-
+  app.use('/products', productsRouter);
 }
+
+module.exports = routerApi;
