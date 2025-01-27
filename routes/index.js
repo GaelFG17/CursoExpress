2ee52799a36e1408491c68be666ced638b279de9
GaelFG17 <gaelfg1720@gmail.com>
2025-01-27 13:54:10 -0600

    users router

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,13 @@ const { mode } = require('crypto-js');
 const productsRouter = require('./productsRouter');
 const categoriesRouter = require('./categoriesRouter');
 const peopleRouter = require('./peopleRouter');
+const usersRouter = require('./usersRouter');
 
 function routerApi(app){
   app.use('/products', productsRouter);
   app.use('/categories', categoriesRouter);
   app.use('/people', peopleRouter);
+  app.use('/users', usersRouter);
 }
 
 module.exports = routerApi;
