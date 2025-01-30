7f810bf28d1249288368373bc8510ecaa2fdb023
GaelFG17 <gaelfg1720@gmail.com>
2025-01-30 17:01:25 -0600

    metodo para la generacion de productos con faker

--- services/productServices.js
+++ services/productServices.js
@@ -4,6 +4,17 @@ class ProductService{
     this.products = [];
   }
 
+  generate(){
+    const limit = size || 10;
+    for (let index = 0; index < limit; index++) {
+      products.push({
+        name: faker.commerce.productName(),
+        price: parseInt(faker.commerce.price(), 10),
+        image : faker.image.url()
+      });
+    }
+  }
+
   create(){
 
   }
