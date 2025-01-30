5407c60224415e80e53338ac6d24a66685afd3b0
GaelFG17 <gaelfg1720@gmail.com>
2025-01-30 17:00:09 -0600

    Creacion del service de productos

---
+++ services/productServices.js
@@ -0,0 +1,29 @@
+class ProductService{
+
+  constructor(){
+    this.products = [];
+  }
+
+  create(){
+
+  }
+
+  find(){
+
+  }
+
+  findOne(){
+
+  }
+
+  update(){
+
+  }
+
+  delete(){
+
+  }
+
+}
+
+module.exports = ProductService;
