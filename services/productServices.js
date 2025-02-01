c0572dc0f61651e66c48c25f050dc13d69535af3
GaelFG17 <gaelfg1720@gmail.com>
2025-02-01 13:54:17 -0600

    promises

--- services/productServices.js
+++ services/productServices.js
@@ -19,7 +19,7 @@ class ProductService{
     }
   }
 
-  create(data){
+  async create(data){
     const newProduct = {
       id : faker.string.uuid(),
       ...data
@@ -28,15 +28,15 @@ class ProductService{
     return newProduct;
   }
 
-  find(){
+  async find(){
     return this.products;
   }
 
-  findOne(id){
+  async findOne(id){
     return this.products.find(item => item.id === id);
   }
 
-  update(id, changes){
+  async update(id, changes){
     const index = this.products.findIndex(item => item.id === id);
     if(index === -1){
       throw new Error('Product not found');
@@ -48,7 +48,7 @@ class ProductService{
     return this.products[index];
   }
 
-  delete(id){
+  async delete(id){
     const index = this.products.findIndex(item => item.id === id);
     if(index === -1){
       throw new Error('Product not found');
