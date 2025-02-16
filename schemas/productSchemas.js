238659dc3ca62893542459f17932f2f520ea322b
GaelFG17 <gaelfg1720@gmail.com>
2025-02-16 12:10:00 -0600

    creacion del middleware para la validacion

--- schemas/productSchemas.js
+++ schemas/productSchemas.js
@@ -7,13 +7,15 @@ const price = Joi.number().integer().min(10);
 const createProductSchema = Joi.object({
     name : name.required(),
     price : price.required()
-})
+});
 
 const updateProductSchema = Joi.object({
     name : name,
     price : price
-})
+});
 
 const getProductSchema = Joi.object({
     id : id.required()
-})
+});
+
+module.exports = {createProductSchema, updateProductSchema, getProductSchema};
